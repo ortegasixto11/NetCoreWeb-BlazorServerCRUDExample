39387e199911458adcc1a17dc465bf2a6cf1cfa3
Sixto Ortega <sixto_miguel@hotmail.com>
2020-03-17 18:01:35 -0400

    Adding Repository to the app

---
+++ BlazorServerCRUDExample/Repositories/ProductRepository.cs
@@ -0,0 +1,50 @@
+﻿using BlazorServerCRUDExample.Data;
+using BlazorServerCRUDExample.Models;
+using BlazorServerCRUDExample.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerCRUDExample.Repositories
+{
+    public class ProductRepository : IProductService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductRepository(ApplicationDbContext context)
+        {
+            //_context = new ApplicationDbContext();
+            _context = context;
+        }
+
+        public async Task Add(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(Guid id)
+        {
+            _context.Products.Remove(await _context.Products.Where(x => x.ID == id).FirstOrDefaultAsync());
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Product>> Get()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<Product> Get(Guid id)
+        {
+            return await _context.Products.Where(x => x.ID == id).FirstOrDefaultAsync();
+        }
+
+        public async Task Update(Product product)
+        {
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ BlazorServerCRUDExample/Services/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using BlazorServerCRUDExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerCRUDExample.Services
+{
+    public interface IProductService
+    {
+        Task<List<Product>> Get();
+        Task<Product> Get(Guid id);
+        Task Add(Product product);
+        Task Update(Product product);
+        Task Delete(Guid id);
+    }
+}
