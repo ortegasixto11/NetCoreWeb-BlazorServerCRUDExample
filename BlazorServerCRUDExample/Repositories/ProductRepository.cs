0a173807be261734485419f3da2e9fee2f2a65b2
Sixto Ortega <sixto_miguel@hotmail.com>
2020-03-23 11:52:25 -0400

    Refactoring Pages

--- BlazorServerCRUDExample/Repositories/ProductRepository.cs
+++ BlazorServerCRUDExample/Repositories/ProductRepository.cs
@@ -21,35 +21,35 @@ namespace BlazorServerCRUDExample.Repositories
 
         public async Task AddAsync(Product product)
         {
-            _context.Products.Add(product);
+            _context.BlazorServerCRUD_Products.Add(product);
             await _context.SaveChangesAsync();
         }
 
         public async Task DeleteAsync(Product product)
         {
-            _context.Products.Remove(product);
+            _context.BlazorServerCRUD_Products.Remove(product);
             await _context.SaveChangesAsync();
         }
 
         public async Task DeleteByIdAsync(Guid id)
         {
-            _context.Products.Remove(await _context.Products.Where(x => x.ID == id).FirstOrDefaultAsync());
+            _context.BlazorServerCRUD_Products.Remove(await _context.BlazorServerCRUD_Products.Where(x => x.ID == id).FirstOrDefaultAsync());
             await _context.SaveChangesAsync();
         }
 
         public async Task<List<Product>> GetAllAsync()
         {
-            return await _context.Products.ToListAsync();
+            return await _context.BlazorServerCRUD_Products.ToListAsync();
         }
 
         public async Task<Product> GetAsync(Guid id)
         {
-            return await _context.Products.Where(x => x.ID == id).FirstOrDefaultAsync();
+            return await _context.BlazorServerCRUD_Products.Where(x => x.ID == id).FirstOrDefaultAsync();
         }
 
         public async Task UpdateAsync(Product product)
         {
-            _context.Products.Update(product);
+            _context.BlazorServerCRUD_Products.Update(product);
             await _context.SaveChangesAsync();
         }
     }
